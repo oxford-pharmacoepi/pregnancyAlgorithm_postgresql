cc8f2df64e789c76cf87a22e019e24303184e68b
tburkard <tiozab@yahoo.com>
2023-11-03 09:51:40 +0000

    update codeToRun

--- extras/CodeToRun.R
+++ extras/CodeToRun.R
@@ -9,10 +9,12 @@ password   <-Sys.getenv("DB_PASSWORD")
 port       <-Sys.getenv("DB_PORT")
 host       <-Sys.getenv("DB_HOST")
 
-# Connect to the database
 
-#connectionDetails <-DatabaseConnector::downloadJdbcDrivers("postgresql",here::here())
 
+# connectionDetails <-DatabaseConnector::downloadJdbcDrivers("postgresql",here::here())
+
+
+# Connect to the database via database connector to run the algorithm
 connectionDetails <-DatabaseConnector::createConnectionDetails(dbms = "postgresql",
                                                                server = server,
                                                                user = user,
@@ -22,39 +24,14 @@ connectionDetails <-DatabaseConnector::createConnectionDetails(dbms = "postgresq
 
 
 targetDialect <-"postgresql"
-cdmDatabaseSchema <-"public_100k"
-vocabularyDatabaseSchema <-"public_100k"
+cdmDatabaseSchema <-"public"
+vocabularyDatabaseSchema <-"public"
 resultsDatabaseSchema <-"results"
 
 connection = connect(connectionDetails)
 #disconnect(connection)
 
-source("~/PregnancyAlgorithm/R/main.R")
+source("~/pregnancyAlgorithm_postgresql/R/main.R")
 init (connectionDetails, resultsDatabaseSchema, useMppBulkLoad = FALSE)
 execute (connectionDetails, cdmDatabaseSchema, resultsDatabaseSchema)
 
-## connect to a cdm instance
-server_dbi <- Sys.getenv("DB_SERVER_DBI_gold_202207")
-user       <- Sys.getenv("DB_USER")
-password   <- Sys.getenv("DB_PASSWORD")
-port       <- Sys.getenv("DB_PORT")
-host       <- Sys.getenv("DB_HOST")
-
-library(CDMConnector)
-library(dplyr)
-
-db <- dbConnect(RPostgres::Postgres(),
-                dbname = server_dbi,
-                port = port,
-                host = host,
-                user = user,
-                password = password
-)
-
-cdm <- cdm_from_con(
-  db,
-  cdm_schema = "public_100k",
-  write_schema = "results"
-)
-
-cdm$motherTable <- tbl(db, in_schema("results","pregnancy_episodes"))
--- extras/post_process.R
+++ extras/post_process.R
@@ -1,7 +1,36 @@
 ## change the motherTable to the correct column names and concept ids
 
 
-# unique(motherTable$original_outcome) : "SA"    "DELIV" "AB"    "LB"    "ECT"   "SB"   
+## connect to a cdm instance to do the postprocessing
+server_dbi <- Sys.getenv("DB_SERVER_DBI_gold_202207")
+user       <- Sys.getenv("DB_USER")
+password   <- Sys.getenv("DB_PASSWORD")
+port       <- Sys.getenv("DB_PORT")
+host       <- Sys.getenv("DB_HOST")
+
+library(CDMConnector)
+library(dplyr)
+
+db <- dbConnect(RPostgres::Postgres(),
+                dbname = server_dbi,
+                port = port,
+                host = host,
+                user = user,
+                password = password
+)
+
+cdm <- cdm_from_con(
+  db,
+  cdm_schema = "public",
+  write_schema = "results"
+)
+
+cdm$motherTable <- tbl(db, in_schema("results","pregnancy_episodes"))
+
+
+# motherTable <- cdm$motherTable %>% collect()
+# unique(motherTable$original_outcome) 
+## "SA"    "DELIV" "AB"    "LB"    "ECT"   "SB"   
 
 # explor <- motherTable %>% select("original_outcome","outcome") %>% distinct(original_outcome,outcome)
 
